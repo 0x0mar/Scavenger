13f403e2a3e942c5016c2b8d253bb1f34f544c94
jaredestroud <jaredestroud@gmail.com>
2014-12-24 01:13:56 -0500

    Fixed the class. Now onto argparser for main.

--- ShodanSearch.py
+++ ShodanSearch.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 __author__ = "Jared Stroud"
 
+from shodan import WebAPI
+
 class Shodan:
 
-	def srchShodan(search_term):
+	def __init__(self):
+		self.SHODAN_API_KEY = "" #Enter API key here
+		self.api = WebAPI(self.SHODAN_API_KEY)
+
+		print("Successfully called Shodan")
+
+	def srchShodan(self, search_term):
 		results = api.search(search_term) #searching based on users preset terms
 		print (shosearch, 'resulted in %s results' % results ['total'])
 		
@@ -14,9 +22,9 @@ class Shodan:
 
 #Name: shodanHost
 #Function: searches IP address, country info, banners, ports etc...
-	def shodanHost(ip):
+	def shodanHost(self, ip):
 
-		host = api.host(ip) #looks up geo location (sometimes will throw back "none" if nothing is recorded in database"
+		host = self.api.host(ip) #looks up geo location (sometimes will throw back "none" if nothing is recorded in database"
 		
 		print """ 
 			IP: %s
